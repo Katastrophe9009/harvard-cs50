af7c66e186a9041518f27ab73a0b97868c48c445
Katastrophe9009 <joshguidry1@gmail.com>
2026-03-19 08:25:43 +0000

    finished extensions.py

--- problem set 1/extensions.py
+++ problem set 1/extensions.py
@@ -1,9 +1,21 @@
 def main():
-    file = input("File name: ").casefold().strip().split(".")
-    print(file)
-    match file:
+    file = input("File name: ").casefold().strip()
+    extension = file.split(".")[-1]
+    match extension:
         case "gif":
             print("image/gif")
+        case "jpg" | "jpeg":
+            print("image/jpeg")
+        case "png":
+            print("image/png")
+        case "pdf":
+            print("application/pdf")
+        case "txt":
+            print("text/plain")
+        case "zip":
+            print("application/zip")
+        case _:
+            print("application/octet-stream")
 
 
 
