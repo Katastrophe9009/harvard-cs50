c51278344b2e9c94ca199feea6ca14b796587989
Katastrophe9009 <joshguidry1@gmail.com>
2026-03-30 12:05:56 +0000

    finished grocery.py

---
+++ problem set 3/grocery.py
@@ -0,0 +1,25 @@
+def main():
+    while True:
+        try:
+            item = input().upper()
+            add_item(item)
+        except KeyError:
+            print("Invalid input")
+        except EOFError:
+            show_list(grocery_list)
+
+def add_item(item):
+    if item in grocery_list:
+        grocery_list[item] += 1
+        return grocery_list
+    else:
+        grocery_list[item] = 1
+        return grocery_list
+
+def show_list(grocery_list):
+    sorted_list = sorted(grocery_list)
+    for item in sorted_list:
+        print(item, grocery_list[item])
+
+grocery_list = {}
+main()
